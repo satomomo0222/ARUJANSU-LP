8ea22d894de53060c63993b116e7c4c1ac36f5d3
satomomo0222 <satomomo0222@gmail.com>
2020-10-24 12:56:53 +0900

    アニメーション完了！

--- js/script.js
+++ js/script.js
@@ -45,16 +45,7 @@ $(window).scroll(function(){
   });
 });
 
-// $(window).scroll(function(){
-//   $('.panel-fadein').each(function(){
-//     var elemPos = $(this).offset().top,
-//         scroll = $(window).scrollTop(),
-//         windowHeight = $(window).height();
-//     if (scroll > elemPos - windowHeight +100){
-//       $(this).addClass('fadein');
-//     }
-//   });
-// });
+
 $(window).scroll(function(){
   $('.slick').each(function(){
     var elemPos = $(this).offset().top,
@@ -128,4 +119,25 @@ $(function() {
 
 
 
+// 回転アニメーション
+$(function() {
+  var rotate = function(circle, angle) {
+    circle.css({
+   "transform" : "rotate("+angle+"deg)"
+   });
+  }
+  $(window).scroll(function(){
+   rotate($(".circle"), $(window).scrollTop());
+  })
+ });
 
+ $(window).scroll(function(){
+  $('.main__sec4 .container__content--text').each(function(){
+    var elemPos = $(this).offset().top,
+        scroll = $(window).scrollTop(),
+        windowHeight = $(window).height();
+    if (scroll > elemPos - windowHeight +100){
+      $(this).addClass('fadein');
+    }
+  });
+});
